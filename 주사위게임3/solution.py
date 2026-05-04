f697c6b3e38cad540bee7f6c77125be960b5c297
syh382 <tojonggo@gmail.com>
2026-05-05 05:38:59 +0900

    Add files via upload

---
+++ 주사위게임3/solution.py
@@ -0,0 +1,23 @@
+def solution(a, b, c, d):# lis = [ex.count(i) for i in ex]로 선언했으면 좀더 깔끔하게 작성가능한 사실을 다른사람들의 풀이를보고 알게됨.
+    ex = [a,b,c,d]
+    lis = [0]*6
+    for i in ex:
+        lis[i-1] += 1
+
+    if 4 in lis: answer = 1111*(lis.index(4)+1)
+
+    elif 3 in lis: answer = (10 * (lis.index(3)+1) + (lis.index(1)+1))**2
+
+    elif lis.count(2) == 2:
+        p = lis.index(2) + 1
+        lis.pop(lis.index(2))
+        q = lis.index(2) + 2
+        answer = (p+q)*abs(p-q)
+
+    elif lis.count(2) == 1:
+        answer = lis.index(1) + 1
+        lis.pop(lis.index(1))
+        answer *= (lis.index(1) + 2)
+
+    elif max(lis) == 1: answer = lis.index(1) + 1
+    return answer
