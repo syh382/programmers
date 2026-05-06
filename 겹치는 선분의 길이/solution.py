4b15e9b9f9be07d86353a891387a95808e7194ca
syh382 <tojonggo@gmail.com>
2026-05-07 04:19:03 +0900

    구현 실패_정답률90%_재도전 예정

--- 겹치는 선분의 길이/solution.py
+++ 겹치는 선분의 길이/solution.py
@@ -1,8 +1,12 @@
+from itertools import combinations
 def solution(lines):
-    a = 0
-    [[x1, x2], [x3, x4], [x5, x6]] = lines
-    print(min(x2,x4) - max(x1,x3))
-    print(a)
-    
-print(solution([[0, 2], [-3, -1], [-2, 1]]))
-# 공식 min(x2,x4) - max(x1,x3) 이 0보다 클때 값이 겹치는 길이
+    lis = [0] * 201 # index 0 ~ 200 == -100 ~ 100
+    for line1,line2 in combinations(lines, 2):
+        ma, mi = max(line1[0], line2[0]), min(line1[1], line2[1])# 공식 min(x2,x4) - max(x1,x3) 이 0보다 클때 값이 겹치는 길이
+        if mi - ma > 0:
+            for i in range(ma, mi+1):
+                lis[i+100] = 1
+            lis[i+100] = 2
+    return lis.count(1)
+# 구현 실패_정답률 90% (논리 구조 오류 미해결, 정답 확인)
+# 재도전 예정 (선을 [0,1] 각 칸에 쌓는 방식으로 구현할것)
