acdb81d5f15926b6fa8501b018874f75c9a52941
syh382 <tojonggo@gmail.com>
2026-05-06 23:53:05 +0900

    vscode commit test

--- 겹치는 선분의 길이/solution.py
+++ 겹치는 선분의 길이/solution.py
@@ -2,6 +2,7 @@ def solution(lines):
     a = 0
     [[x1, x2], [x3, x4], [x5, x6]] = lines
     print(min(x2,x4) - max(x1,x3))
+    print(a)
     
 print(solution([[0, 2], [-3, -1], [-2, 1]]))
 # 공식 min(x2,x4) - max(x1,x3) 이 0보다 클때 값이 겹치는 길이
