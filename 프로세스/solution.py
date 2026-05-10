3d32f5da506a242c2a9e2c60f5d03e8355ecd40d
syh382 <tojonggo@gmail.com>
2026-05-10 22:15:00 +0900

    나중에 다른사람 풀이 볼예정

--- 프로세스/solution.py
+++ 프로세스/solution.py
@@ -6,3 +6,6 @@ def solution(priorities, location):
             a += 1
             if type(priorities.pop(0)) == float: return a
         else: priorities.append(priorities.pop(0))
+# 다른사람의 풀이는 다음에 봃예정
+# 주관적 난이도 하
+# https://school.programmers.co.kr/learn/courses/30/lessons/42587
