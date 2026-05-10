c7c018d86028092e9f56415b792c30c1c126d5e6
syh382 <tojonggo@gmail.com>
2026-05-10 19:38:53 +0900

    Update solution.py

--- 택배 상자 꺼내기/solution.py
+++ 택배 상자 꺼내기/solution.py
@@ -15,3 +15,4 @@ def solution(n, w, num):
     else: return len(lis) - y -1
 # 다른사람의 풀이에 패턴을 분석해 반복문 없이 짧게 푸는 방법이 있음..
 # 주관적 난이도 : 하
+# https://school.programmers.co.kr/learn/courses/30/lessons/389478
