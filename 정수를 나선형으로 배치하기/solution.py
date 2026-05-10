aff4d6cc4f79d159bac7681e79bd47ced55c8a5a
syh382 <tojonggo@gmail.com>
2026-05-10 18:11:03 +0900

    Update solution.py

--- 정수를 나선형으로 배치하기/solution.py
+++ 정수를 나선형으로 배치하기/solution.py
@@ -26,14 +26,14 @@ def solution_old(n): #코드가 매우 길고 더러워 만족스럽지 않음. 
         lis[y[i]][x[i]] = i+1
     return lis
 def solution(n): # 좌표를 직접 이동하는 좀더 짧은 코드
-    result = [[x for x in range(1,n+1)] for i in range(n)]
-    p = (1,0),(0,-1),(-1,0),(0,1)
-    num,x,y,pnum = n+1,n-1,0,0
+    result = [[x for x in range(1,n+1)] for i in range(n)] # n*n 리스트 생성
+    p = (1,0),(0,-1),(-1,0),(0,1) # 이동할 방향
+    num,x,y,pnum = n+1,n-1,0,0 # 변수 선언  num은 n+1 ~ n * n 까지의 값
     for i in range(1,n):
         if pnum > 3: pnum=0
         for _ in range(2):
             for _ in range(n-i):
-                y, x, result[y][x] = y + p[pnum][0], x + p[pnum][1], num
+                y, x, result[y][x] = y + p[pnum][0], x + p[pnum][1], num # 반복마다 x,y에 이동하는 방향의 값을 더하고 result y,x 좌표에 값 넣어주기
                 num += 1
             pnum +=1
-    return result 
+    return result # 결과 리턴
