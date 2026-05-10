f27159c23b7a58fa0b2bac5edf1bcae7fec9d2c7
syh382 <tojonggo@gmail.com>
2026-05-10 21:10:38 +0900

    .

---
+++ 외계어 사전/solution.py
@@ -0,0 +1,11 @@
+def solution(spell, dic):
+    for i in dic:
+        for ii in spell:
+            if ii in i:
+                if spell.index(ii) == len(spell)-1:
+                    return 1
+            else:
+                break
+    return 2
+# spell = set(spell) spell - set(dic) 방식으로 더욱 간단하게 풀이 가능
+# 주관적 난이도 최하
