65080ea29ffd6108b8bfd1e62cf5dbc44a8727dc
syh382 <tojonggo@gmail.com>
2026-05-11 18:07:24 +0900

    deque 사용 코드 추가

--- 프로세스/solution.py
+++ 프로세스/solution.py
@@ -7,8 +7,6 @@ def solution(priorities, location):
             a += 1
             if type(priorities.pop(0)) == float: return a
         else: priorities.append(priorities.pop(0))
-print(solution([2, 1, 3, 2],2),time.time()-start)
-# 다른사람의 풀이는 다음에 봃예정
 # 주관적 난이도 하
 # https://school.programmers.co.kr/learn/courses/30/lessons/42587
 def solution2(p, location):
@@ -16,9 +14,9 @@ def solution2(p, location):
     p = deque(p)
     p[location] = float(p[location])
     while len(p) > 0:
-        if p[0] >= max(p):
+        if any(p[0] < i for i in p): p.rotate(-1)
+        else: 
             a += 1
             if isinstance(p.popleft(),float): return a
-        else: p.rotate(-1)
-# collections의 deque를 사용한 코드.
-# 시간복잡도를 n^2에서 n으로 줄임
+# collections의 deque 사용코드
+
