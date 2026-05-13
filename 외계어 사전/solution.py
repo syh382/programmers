ab1d73ea2e38d2b2c00070c0a0f355b1a328648c
syh382 <tojonggo@gmail.com>
2026-05-13 18:52:42 +0900

    .

--- 외계어 사전/solution.py
+++ 외계어 사전/solution.py
@@ -1,11 +1,11 @@
 def solution(spell, dic):
-    for i in dic:
-        for ii in spell:
-            if ii in i:
-                if spell.index(ii) == len(spell)-1:
-                    return 1
-            else:
-                break
+    for i in dic: # "sod", "eocd", "qixm", "adio", "soo"
+        for ii in spell: # "p", "o", "s"
+            if ii in i: # ii가 i안에 포함되어 있을 때 True
+                if ii == spell[-1]: # ii의 값이 spell의 맨 뒤 인덱스와 동일할때
+                    return 1 
+            else: # i안에 포함되지 않는 ii가 나올시 반복 중단
+                break 
     return 2
 # spell = set(spell) spell - set(dic) 방식으로 더욱 간단하게 풀이 가능
 # 주관적 난이도 최하
