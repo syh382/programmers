12dd6dc079e66d9bb2c86b0a33c3e3ef48134b4f
syh382 <tojonggo@gmail.com>
2026-05-14 01:22:44 +0900

    .

--- 연속된 수의 합/solution.py
+++ 연속된 수의 합/solution.py
@@ -1,6 +1,6 @@
 def solution(num, total):
-    a = [x for x in range(-49,101)]
-    for i in range(150-num):
-        e = a[i:i+num]
-        if sum(e) == total: return e
-print(solution(4,2))
+    a = 1 if num%2 == 0 else 0
+    return  [x for x in range(total // num - num//2 + a, total // num + num//2 + 1)]
+# 다른사람들의 풀이를 보면 등차수열로 푸는 방법이 있다.
+# 주관적 난이도 중
+# https://school.programmers.co.kr/learn/courses/30/lessons/120923
