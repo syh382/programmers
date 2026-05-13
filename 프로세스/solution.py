35c3a9d115a084c096b74d3dceb5c938e9d97805
syh382 <tojonggo@gmail.com>
2026-05-13 20:38:44 +0900

    .

--- 프로세스/solution.py
+++ 프로세스/solution.py
@@ -2,7 +2,7 @@ from collections import deque
 def solution(priorities, location):
     a = 0
     priorities[location] = float(priorities[location])
-    while len(priorities) > 0:
+    while priorities:
         if priorities[0] >= max(priorities):
             a += 1
             if type(priorities.pop(0)) == float: return a
