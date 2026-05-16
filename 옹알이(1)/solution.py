e1c262a407c48427f40a0970405921f90daa7449
syh382 <tojonggo@gmail.com>
2026-05-16 18:14:52 +0900

    .

--- 옹알이(1)/solution.py
+++ 옹알이(1)/solution.py
@@ -1,13 +1,10 @@
 def solution(babbling):
     result = 0
-    for i,s in enumerate(babbling): # i = index, s = babbling[i]
+    for i in babbling:
         for ii in ["aya","ye","woo","ma"]:
-            if ii in s:
-                babbling[i] = babbling[i].replace(ii, " ") # 일치하는 부분을 공백으로 대체
-        babbling[i] = babbling[i].strip() # 공백 제거
-        if len(babbling[i]): # 리스트 안에 값이 남아있으면 발음할수 있는것으로 간주
-            continue
-        result+=1 # 리스트에 값이 없을때 결과 + 1
+            if ii in i: i = i.replace(ii, " ") # 일치하는 부분을 공백으로 대체
+        if i.strip(): continue # a 안에 값이 남아있으면 발음할수 없는것으로 간주
+        result+=1 # 문자열 a에 값이 없을때 결과 + 1
     return result
 # 주관적 난이도 하 
 # 공백 제거를 replace(" ", "")가 아닌 strip()으로 더 깔끔(?) 하게 하기
